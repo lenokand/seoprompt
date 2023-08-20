ee767329137f87a61e2d6f3e8d6f5c955b786d2a
Elena <72378305+lenokand@users.noreply.github.com>
2023-08-20 19:14:17 +0200

    parts

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const swiper = new Swiper('.result-slider', {
+    speed: 400,
+    spaceBetween: 100,
+    nextButton: '.result-slider .swiper-next',
+    prevButton: '.result-slider .swiper-prev',
+  });
