66b20966cb6fd59296ddbda4b52d14c077e9c026
Elena <72378305+lenokand@users.noreply.github.com>
2023-08-20 22:34:30 +0200

    slider

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 const swiper = new Swiper('.result-slider', {
     speed: 400,
     spaceBetween: 100,
-    nextButton: '.result-slider .swiper-next',
-    prevButton: '.result-slider .swiper-prev',
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    navigation: {
+      nextEl: '.result-slider .swiper-next',
+      prevEl: '.result-slider .swiper-prev',
+    },
+    // nextButton: '.result-slider .swiper-next',
+    // prevButton: '.result-slider .swiper-prev',
+    breakpoints: {
+      // when window width is >= 320px
+      // 320: {
+      //   slidesPerView: 2,
+      //   spaceBetween: 20
+      // },
+      // // when window width is >= 480px
+      // 480: {
+      //   slidesPerView: 3,
+      //   spaceBetween: 30
+      // },
+      // // when window width is >= 640px
+      // 640: {
+      //   slidesPerView: 4,
+      //   spaceBetween: 40
+      // }
+    }
   });
